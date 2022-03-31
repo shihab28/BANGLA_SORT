d058ffad1814d5e9f770bb3d17435973d265b7e7
shihab28 <shihab.ahamad28@gmail.com>
2022-03-31 18:22:44 +0600

    "."

--- resource/PATTERN_JSON.py
+++ resource/PATTERN_JSON.py
@@ -9,7 +9,8 @@ with open(jsonPath, 'r', encoding='utf-8') as jo:
     jsonOb = json.load(jo)
 
 # print(jsonOb.keys())
-mainDict = json.loads(jsonOb)
+# mainDict = json.loads(jsonOb)
+mainDict = jsonOb
 # print(mainDict)
 
 new_dictionary = {}
@@ -30,11 +31,18 @@ for root in list(new_dictionary["grapheme_root"].keys()):
         'v': -1
     }
 
+# grapheme_root, consonant_diacritic, vowel_diacritic
 
-for root in list(new_dictionary["grapheme_root"].keys())[13:]:
+leastList = ['1', '8', '9']
+for root in list(new_dictionary["grapheme_root"].keys())[11:]:
     for consonant in new_dictionary["consonant_diacritic"].keys():
         for vowel in new_dictionary["vowel_diacritic"].keys():
-            pattern_value = root + vowel + consonant
+
+            if consonant in leastList:
+                pattern_value = root + vowel + consonant
+            else:
+                pattern_value = root + consonant + vowel
+                
             pattern_dictionary[pattern_value] = {
                 'c': new_dictionary["consonant_diacritic"][consonant],
                 'r': new_dictionary["grapheme_root"][root],
--- resource/formatInputJson.py
+++ resource/formatInputJson.py
@@ -5,8 +5,25 @@ curDir = os.path.dirname(__file__).replace("\\", "/")
 jsonPath = f"{curDir}/class_map_corrected.json".replace("\\", "/")
 
 with open(jsonPath, 'r', encoding = 'utf-8') as rf:
-    jsonOb = json.load(rf)
+    jsonOb = json.load(rf)  
 
-jsonDict = json.dumps(jsonOb)
-for keyD in jsonDict:
-    print(jsonDict[keyD])
+    try: 
+        jsonDict = json.loads(jsonOb)
+    except:
+        jsonDict = jsonOb
+
+# print(jsonOb)
+# for keyD in jsonDict:
+#     if keyD == 'grapheme_root':
+#         tempDict = {}
+#         tempIndList = list(jsonDict[keyD].keys())
+#         for ind, keyS in enumerate(jsonDict[keyD]):
+#             if ind >= 2:
+#                 tempDict[tempIndList[ind - 2]] = jsonDict[keyD][keyS]
+
+#         jsonDict[keyD] = tempDict
+
+
+# with open(jsonPath, 'w', encoding = 'utf-8') as wf:
+#     json.dump(jsonDict, wf)
+# wf.close()
